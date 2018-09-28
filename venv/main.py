98e51ffdc18abb4218a721b7dc4f3e5107059ae5
claythearc <clay-turner@hotmail.com>
2018-09-27 19:57:32 -0500

    part c changes

--- venv/main.py
+++ venv/main.py
@@ -4,34 +4,54 @@
 
 from math import floor, ceil, sqrt
 import time
-import timeit
-import hashlib
+import random
 
 class HashTable:
+    """Class for holding the hash table."""
     def __init__(self):
+        """Initialize a dictionary as our base data structure.
+            Not using the hash style of dictionaries per se, it's just the best assosciation
+            of Key & Value.
+         """
         self.values = dict()
 
-    def append(self,value):
-        key = hashlib.md5(value.encode()).hexdigest()
+    def append(self,value: str):
+        """Append a value to our dictionary.
+        Get the MD5 hash of the value to be inserted. Sets that as the key
+         and the plaintext value as the value. If the key already exists, it raises a Key Error."""
+        key = hash(value)
         if key in self.values.keys():
-            raise KeyError("Not Unique!")
+            pass
         self.values[key] = value
 
     def __str__(self):
+        """Function for overriding the string output, outputs as { x, y, z}"""
         temp = []
         for k,v in self.values.items():
             temp.append(v)
-        temp = "{ " + ", ".join(value for value in temp) + " }"
+        temp = "{ " + ", ".join(str(value) for value in temp) + " }"
         return temp
 
     def get(self, value):
-        return self.values[hashlib.md5(value.encode()).hexdigest()]
+        """Gets the value assosciated with the key. Sanity check basically."""
+        value = hash(value)
+        return self.values[value]
 
     def __contains__(self, item):
-        return self.values[hashlib.md5(item.encode()).hexdigest()]
-
+        """Overrides the X in Y declaration. Basically checks if the hash exists as a key"""
+        item = hash(item)
+        return self.values.get(item, False)
+    
+    
+    def hash(self, value):
+        templist = str(value)
+        templist = list(templist)
+        temp = 0
+        for item in templist:
+            temp += (temp ** item) + 1
 
 def mulinv(a: int, b: int, debug: bool = False):
+    """Function to calculate the multiplicative inverse."""
     # initialize other variables
     q = 0
     x = 0
@@ -51,14 +71,6 @@ def mulinv(a: int, b: int, debug: bool = False):
 
 
 def main():
-
-    foo = HashTable()
-    foo.append("bla")
-    foo.append("bar")
-    print(foo.get("bar"))
-    print(foo)
-    exit()
-
     a = int(input("Input Value of A: "))  # type: int
     n = int(input("Input Value of N: "))  # type: int
     b = int(input("Input Value of B: "))  # type: int
@@ -76,31 +88,18 @@ def main():
     # Problem looks like A^x is congruent to b mod n
 
     # Build the list of A^i mod n for i = [0, m)
+    Hashes = HashTable()
     for i in range(0, m):
-        Idx_List.append(pow(a, i, n))
-    Idx_List.sort()  # sort the list
-    print(f"Sorted Array: {Idx_List}")
+        Hashes.append(pow(a, i, n))
+    print(f"Hash Table: {Hashes}")
     # Here we take b * (a inverse)^i mod n and find i
     # By taking the Array above, and seeing if the item exists in the table.
 
-    idx = False
     for i in range(0, m):
         # solve the equation for B * (a^-1)^i
         temp = (b * (inv**i)) % n
-        first = 0
-        last = len(Idx_List)
-        # binary search. loop through, move midpoint to middle each orientation, if it's less check the bottom half
-        # if it's more, check the top half. repeat until you're out of list or it's found
-        while (first <= last) and (not idx):
-            mid = (first+last)//2
-            if Idx_List[mid] == temp:
-                idx = True
-            else:
-                if temp < Idx_List[mid]:
-                    last = mid - 1
-                else:
-                    first = mid + 1
-        if idx:
+        # Due to the override of __contains__ I can check membership through temp in hashes.
+        if temp in Hashes:
             print(f"The Answer is: {i} Giving us {b}*{inv}^{i} \n")
             break
 
